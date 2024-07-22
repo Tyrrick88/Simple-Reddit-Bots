7a878e2832f2df1eb332c95edf786ddacbccba6e
Tyrrick <tyrrickndungu@gmail.com>
2024-07-22 17:51:08 +0300

    FIX:Fixed the password prompt to mask the pasword after Entering it

--- Ruto/config.py
+++ Ruto/config.py
@@ -1,7 +1,10 @@
+from getpass import getpass
+
+
 username = ("Different_Fox_***")
-password = (":*$%3Z%mL.j~****")
+password = getpass("Please Enter your password: ")
 client_id = ("y8XLshwe60HZfBWW16w***")
 client_secretKey = ("DTNwMPPkDckPUO1Xj8yv-*******")
-
-
-
+Keywords = ("dogs")
+subreddit = ("Kenya")
+message = ("")
